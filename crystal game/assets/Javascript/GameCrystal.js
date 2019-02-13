e9c463eaafd9b85ee89354f341f974ad95e3abfa
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2019-02-13 03:18:14 -0500

    update not done

--- crystal game/assets/Javascript/GameCrystal.js
+++ crystal game/assets/Javascript/GameCrystal.js
@@ -62,7 +62,7 @@ var numberOptions = [10, 5, 3, 7];
     for (var i = 0; i < numberOptions.length; i++) {
         var imageCrystal = $("<img>");
         imageCrystal.addClass("crystal");
-        imageCrystal.attr("src","http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+        imageCrystal.attr("src","crystal");
         imageCrystal.attr("data-crystalvalue", numberOptions[i]);
         $("#crystals").append(imageCrystal);
     }
